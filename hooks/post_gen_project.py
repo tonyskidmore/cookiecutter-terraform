16566e4ae63a4208d162afe7ece9e1eae1daa010
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-07-23 15:44:42 +0100

    fixing post gen script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -29,8 +29,6 @@ def clean_extra_files():
                 os.remove(file_or_dir)
             else:
                 shutil.rmtree(file_or_dir)
-        shutil.copy(".env.example", ".env")
-        open("dev.db", 'a').close()
     except OSError as e:
         _logger.warning("While attempting to remove file(s) an error occurred")
         _logger.warning(f"Error: {e}")
