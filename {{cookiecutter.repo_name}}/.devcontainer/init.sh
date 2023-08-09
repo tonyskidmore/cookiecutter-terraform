6bf2f8f91dde219db42c705bda834ec6e0fe3245
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-08-09 21:15:31 +0100

    adding git init

--- {{cookiecutter.repo_name}}/.devcontainer/init.sh
+++ {{cookiecutter.repo_name}}/.devcontainer/init.sh
@@ -5,6 +5,7 @@ owner=$(stat -c '%U' "$HOME/.pre-commit")
 
 if [[ "$owner" == "root" ]]
 then
+  [[ ! -d .git ]] && git init
   sudo chown vscode:vscode "$HOME/.pre-commit"
   pre-commit install
   pre-commit install-hooks
